1dfa138e607be85aa9352ce7674d61dfae549a9b
Tiyas Kar <tiyas.kar@vphrase.com>
2024-06-17 12:36:37 +0530

    fixed cmd in script

--- scripts/.backend_ports.sh
+++ scripts/.backend_ports.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 tmux new-session -d -s backend
-tmux new -d -s backend 'kubectl port-forward --address 0.0.0.0 $(kubectl get po --selector=app=backend -o=name) 8080:8080'
+tmux send-keys -t backend 'kubectl port-forward --address 0.0.0.0 $(kubectl get po --selector=app=backend -o=name) 8080:8080' C-m
 tmux attach-session -t backend
--- scripts/.frontend_ports.sh
+++ scripts/.frontend_ports.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 tmux new-session -d -s frontend
-tmux new -d -s frontend "kubectl port-forward --address 0.0.0.0 $(kubectl get po --selector=app=frontend -o=name) 8081:80"
+tmux send-keys -t frontend "kubectl port-forward --address 0.0.0.0 $(kubectl get po --selector=app=frontend -o=name) 8081:80" C-m
 tmux attach-session -t frontend
